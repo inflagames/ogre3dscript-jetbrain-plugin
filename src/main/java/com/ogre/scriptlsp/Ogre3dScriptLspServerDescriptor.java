a45e154584bf683226d73add112dd2b9b832524e
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2024-02-17 14:10:20 +0100

    Updated LSP linux binary and update logic to copy binary if checksum is different

--- src/main/java/com/ogre/scriptlsp/Ogre3dScriptLspServerDescriptor.java
+++ src/main/java/com/ogre/scriptlsp/Ogre3dScriptLspServerDescriptor.java
@@ -6,12 +6,21 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor;
 import com.intellij.platform.lsp.api.customization.LspFormattingSupport;
+import com.jediterm.terminal.TerminalDataStream.EOF;
 import com.jgoodies.common.base.SystemUtils;
 import com.ogre.scriptlsp.exceptions.CopyLspAppException;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
+import java.util.Objects;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -34,7 +43,7 @@ public class Ogre3dScriptLspServerDescriptor extends ProjectWideLspServerDescrip
     // copy lsp to tmp files before being executed
     String executablePath = copyLspToSystem();
 
-      return new GeneralCommandLine(Arrays.asList(executablePath, "-l"));
+    return new GeneralCommandLine(Arrays.asList(executablePath, "-l"));
   }
 
   private String copyLspToSystem() throws CopyLspAppException {
@@ -45,24 +54,34 @@ public class Ogre3dScriptLspServerDescriptor extends ProjectWideLspServerDescrip
     if (SystemUtils.IS_OS_LINUX) {
       executablePath = System.getProperty("user.home") + "/.local/bin/ogre_scripts_LSP";
       resourceBinary = "ogre_scripts_LSP";
-    }  else if (SystemUtils.IS_OS_WINDOWS) {
+    } else if (SystemUtils.IS_OS_WINDOWS) {
       executablePath = System.getProperty("user.home") + "\\AppData\\Local\\Programs\\ogre_scripts_LSP.exe";
       resourceBinary = "ogre_scripts_LSP.exe";
     } else {
       throw new CopyLspAppException("plugin not supported in OS");
     }
     try {
-      if (!new File(executablePath).exists()) {
-        //noinspection DataFlowIssue
-        FileUtils.copyURLToFile(getClass().getResource("/lsp/" + resourceBinary), new File(executablePath));
+      URL binarySrcUrl = getClass().getResource("/lsp/" + resourceBinary);
+      // copy binary if not exist or if different checksum
+      if (!new File(executablePath).exists() ||
+          !Objects.equals(fileMd5checksum(new FileInputStream(executablePath)), fileMd5checksum(binarySrcUrl.openStream()))) {
+        FileUtils.copyURLToFile(binarySrcUrl, new File(executablePath));
         if (!new File(executablePath).setExecutable(true)) {
           throw new CopyLspAppException("Couldn't make the LSP application executable");
         }
       }
-    } catch (IOException | NullPointerException e) {
+    } catch (IOException | NullPointerException | NoSuchAlgorithmException e) {
       throw new CopyLspAppException("Couldn't copy the LSP (Language Server Protocol) application", e);
     }
     return executablePath;
+  }
+
+  public String fileMd5checksum(InputStream is) throws NoSuchAlgorithmException, IOException {
+    MessageDigest md = MessageDigest.getInstance("MD5");
+    DigestInputStream dis = new DigestInputStream(is, md);
+    while (dis.read() != -1) {
+    }
+    return new String(md.digest());
   }
 
   @Override
